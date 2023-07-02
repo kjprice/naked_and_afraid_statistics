7becf79cacc7cd2508369763f752805c4c0d0b8d
KJ Price <kjprice12@gmail.com>
2023-07-02 13:40:32 -0500

    Install Seaborn

--- bin/conda_create.sh
+++ bin/conda_create.sh
@@ -11,6 +11,6 @@ cd ..
 
 CONDA_ENV_DIR=conda_env
 
-conda create -y --prefix env jupyterlab matplotlib numpy pandas requests openpyxl ipykernel
+conda create -y --prefix env jupyterlab matplotlib numpy pandas requests openpyxl ipykernel seaborn
 ./env/bin/python -m ipykernel install --prefix=env --name='naked_and_afraid'
 # python -m ipykernel install --prefix=env
